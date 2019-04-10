97e21171d039e1287f20cb920f08f541574f9a79
GuoChao <15821069591@168.com>
2019-04-10 22:00:16 +0800

    添加java工程到版本库

---
+++ DvdManage/src/DVD.java
@@ -0,0 +1,15 @@
+import java.util.Date;
+
+public class DVD {
+	
+	//属性
+	String id;//编号
+	String name;//片名
+	String type;//类型
+	boolean status;//是否被借出
+	Date lendTime;//借出时间
+	Date returnTime;//归还时间
+	
+	
+
+}
